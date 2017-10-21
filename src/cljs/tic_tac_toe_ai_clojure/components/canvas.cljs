70d7abeda81a84080f39e5d112d46bc3ad7a8b7d
Angelo Ocana <angeloocana@gmail.com>
2017-10-21 17:52:25 -0400

    Add initial canvas and btn new game

---
+++ src/cljs/tic_tac_toe_ai_clojure/components/canvas.cljs
@@ -0,0 +1,4 @@
+(ns tic-tac-toe-ai-clojure.components.canvas)
+
+(defn component [game selectPosition]
+  [:canvas])
--- src/cljs/tic_tac_toe_ai_clojure/components/game.cljs
+++ src/cljs/tic_tac_toe_ai_clojure/components/game.cljs
@@ -1,15 +1,20 @@
 (ns tic-tac-toe-ai-clojure.components.game  
   (:require    
     [keechma.ui-component :as ui]
-    [tic-tac-toe-ai-clojure.components.score :as score])
+    [tic-tac-toe-ai-clojure.components.score :as score]
+    [tic-tac-toe-ai-clojure.components.canvas :as canvas])
   (:use 
     [tic-tac-toe-ai-clojure.game :only [get-initial-game]]))
 
 (defn render []
-  (let [game (get-initial-game)]
+  (let [game (get-initial-game)
+        selectPosition (fn [])]
   [:section
     [:h1 "Tic Tac Toe AI"]
-    [score/component game]]))
+    [score/component game]
+    [canvas/component game selectPosition]
+    [:div
+      [:button "new-game"]]]))
 
 (def component
   (ui/constructor
