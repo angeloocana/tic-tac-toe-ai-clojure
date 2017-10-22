22c276c49711a17f754bccb94d2d7fde9ab32524
Angelo Ocana <angeloocana@gmail.com>
2017-10-22 12:14:03 -0400

    Add get-percentages, working in animations

--- src/cljs/tic_tac_toe_ai_clojure/utils/canvas.cljs
+++ src/cljs/tic_tac_toe_ai_clojure/utils/canvas.cljs
@@ -1,22 +1,27 @@
 (ns tic-tac-toe-ai-clojure.utils.canvas)
 
-(def get-board-lines  
-  (memoize
-    (fn [w h]
-      "Get board lines [[[fromX fromY] [toX toY]]]
-        w width
-        h height
-        sw square-width"
-      (let [sw (/ w 3)
-            sw2 (* sw 2)]
-        [[[sw  0] [sw  h]] ;; col 1
-         [[sw2 0] [sw2 h]] ;; col 2
-         [[0  sw] [h  sw]] ;; row 1
-         [[0 sw2] [h  sw2]]])))) ;; row 2
-
-
 (defn clear-canvas! [ctx width height]
   (.clearRect ctx 0 0 width height))
+
+;; Animation
+
+(defn add-frame [type frames data]
+  "Concat a new frame to frames"
+  (let [frame {:type type
+               :data data}]
+    (concat frames [frame])))
+
+;; "Example per 20% => [0 20 40 60 80 100]"
+(defn get-percentages 
+  ([per] (get-percentages per [0]))
+  ([per percentages]
+    (let [last-per (last percentages)
+          new-per (+ per last-per)]
+      (if (< new-per 100) 
+        (recur per (concat percentages [new-per]))
+        (concat percentages [100])))))
+
+;; Draw line
 
 (defn draw-line! [theme ctx line]
   "Draw a line to the canvas"
@@ -31,15 +36,67 @@
       (.lineTo ctx x y))
     (.stroke ctx)))
 
+(defn get-line-mid-point [from to per]
+  "Get line middle point by percentage 0.1 === 10%"
+  (let [[from-x from-y] from
+        [to-x to-y] to]
+    [(+ from-x (* per (- to-x from-x)))
+     (+ from-y (* per (- to-y from-y)))]))
+
+(defn add-line-frame [frames line]
+  "Concat a new line frame to frames"
+  (add-frame "line" frames line))
+
+(defn get-line-frames []
+  ; ([per from to] (get-line-frames per from to per []))
+  ; ([per from to i frames]
+  ;   "Split line into small lines to draw by frame"
+  ;   (let [mid-line [(first (last frames))
+  ;                   (get-line-mid-point from to (/ i 100))]]
+  ;     (if (< per 100)
+  ;       (recur per from to (+ i per) (add-line-frame frames mid-line)) 
+  ;       frames))
+        )
+
+(defn get-lines-frames [theme lines]
+  (map (get-line-frame (get theme :percentage-by-frame))))
+
+(defn draw-lines-animation! [theme ctx raf lines]
+  (let [frames (get-lines-frames theme lines)]
+    (draw-animation! theme ctx raf frames)))
+    
+;; Draw board
+
+(def get-board-lines  
+  (memoize
+    (fn [w h]
+      "Get board lines [[[fromX fromY] [toX toY]]]
+        w width
+        h height
+        sw square-width"
+      (let [sw (/ w 3)
+            sw2 (* sw 2)]
+        [[[sw  0] [sw  h]] ;; col 1
+         [[sw2 0] [sw2 h]] ;; col 2
+         [[0  sw] [h  sw]] ;; row 1
+         [[0 sw2] [h  sw2]]])))) ;; row 2
+
 (defn draw-board-lines! [theme ctx w h]
   (let [lines (get-board-lines w h)]
     (doseq [line lines] (draw-line! theme ctx line))))
 
-(defn draw-initial-game! [theme ctx raf game]
-  ;; (draw-board-lines-animation! (get theme :board-lines) ctx raf)
-  )
+(defn draw-board-lines-animation! [theme ctx raf w h]
+  (let [lines (get-board-lines w h)]
+    (draw-lines-animation! theme ctx raf lines)))
+
+;; Draw game
+
+(defn draw-initial-game! [theme ctx raf game w h]
+  "Draw initial game with animation"
+  (draw-board-lines-animation! (get theme :board-lines) ctx raf w h))
 
 (defn draw-game! [theme ctx raf game w h]
+  "Draw game"
   (let [{:keys [board-lines positions win-line]} theme]
     (draw-board-lines! board-lines ctx w h)
     ;; (draw-positions! positions ctx game)  
@@ -52,6 +109,6 @@
         width (.-clientWidth canvas)
         height (.-clientHeight canvas)]
   (clear-canvas! ctx width height)
-  (if (not (get game :started)) ;; Remove not after draw-board-lines-animation
+  (if (get game :started)
     (draw-game! theme ctx raf game width height)
     (draw-initial-game! theme ctx raf game))))
--- test/cljs/tic_tac_toe_ai_clojure/utils/canvas_test.cljs
+++ test/cljs/tic_tac_toe_ai_clojure/utils/canvas_test.cljs
@@ -2,12 +2,48 @@
   (:require [cljs.test :refer-macros [deftest testing is]]
             [tic-tac-toe-ai-clojure.utils.canvas :as canvas]))
 
+;; Animation
+(deftest get-percentages-20
+  (testing "Get percentages 20"
+    (let [
+      percentage 20
+      expected-percentages [0 20 40 60 80 100]
+      percentages (canvas/get-percentages percentage)]
+      (is (= percentages expected-percentages)))))
+
+;; Draw line
+
+(deftest get-line-frames-0-0-0-100-10
+  (testing "Get line frames"
+    (let [
+      line [[0 0] [0 100]]
+      percentage-by-frame 10
+      expected-lines [
+        { :type "line" :data [[0  0] [0  10]] }
+        { :type "line" :data [[0 10] [0  20]] }
+        { :type "line" :data [[0 20] [0  30]] }
+        { :type "line" :data [[0 30] [0  40]] }
+        { :type "line" :data [[0 40] [0  50]] }
+        { :type "line" :data [[0 50] [0  60]] }
+        { :type "line" :data [[0 60] [0  70]] }
+        { :type "line" :data [[0 70] [0  80]] }
+        { :type "line" :data [[0 80] [0  90]] }
+        { :type "line" :data [[0 90] [0 100]] }
+      ]
+      lines (canvas/get-line-frames percentage-by-frame line)]
+      (is (= lines expected-lines)))))
+
+;; Draw board
+
 (deftest get-board-lines-90-90
   (testing "Get board lines board 90x90"
     (let [
-      actual (canvas/get-board-lines 90 90)
-      expected [[[30 0] [30 90]]
+      lines (canvas/get-board-lines 90 90)
+      expected-lines [[[30 0] [30 90]]
                 [[60 0] [60 90]]
                 [[0 30] [90 30]]
                 [[0 60] [90 60]]]]
-    (is (= actual expected)))))
+    (is (= lines expected-lines)))))
+
+
+;; Draw game
